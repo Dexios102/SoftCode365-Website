3c9a8f7e59aea6bccf11a7fee8d9b6da24c0da33
githubSeven365 <dexterdave.cajayon.1121@gmail.com>
2024-11-12 11:58:24 +0800

    centered icon in service card

--- components/service-card.vue
+++ components/service-card.vue
@@ -1,7 +1,7 @@
 <template>
   <v-col cols="12" class="pa-8 border" style="box-shadow: 0px 8.57px 24px 0px #0000000d">
     <v-row no-gutters class="h-100">
-      <v-col cols="12" class="d-flex ga-2">
+      <v-col cols="12" class="d-flex ga-2 align-center">
         <v-img :src="imgSrc" width="40" height="40" />
         <div>
           <span class="font-w-600 text-m-3 w-100 text-blue-theme">{{ title }}</span>
