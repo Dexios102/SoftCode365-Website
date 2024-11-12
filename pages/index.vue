ba831b39f49dff5a12f3d1822de2775c26726243
githubSeven365 <dexterdave.cajayon.1121@gmail.com>
2024-11-12 09:04:20 +0800

    remove div

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <HeroSectionHero />
-    <ServicesSectionServices />
-    <ClientSectionClient />
-    <PartnerSectionPartner />
-  </div>
+  <HeroSectionHero />
+  <ServicesSectionServices />
+  <ClientSectionClient />
+  <PartnerSectionPartner />
 </template>
