7e6a4c114c2d1b0e312a38204cd4b52026c919c4
githubSeven365 <dexterdave.cajayon.1121@gmail.com>
2024-11-14 15:21:01 +0800

    branches hero

--- components/client-section/branches-hero.vue
+++ components/client-section/branches-hero.vue
@@ -2,19 +2,30 @@
   <div>
     <v-row
       no-gutters
-      class="rounded-e-xl bg-dark-theme h-100 text-white position-relative"
+      class="bg-dark-theme h-100 text-white position-relative"
+      :class="$vuetify.display.mdAndUp ? 'rounded-e-xl' : 'rounded-none'"
     >
       <v-col cols="12" justify="start">
-        <v-row no-gutters>
+        <v-row no-gutters align="center">
           <v-col
-            cols="6"
+            cols="12"
+            md="5"
+            lg="6"
             class="d-flex align-center"
-            style="padding: 6rem 0 6rem 0"
+            :style="
+              $vuetify.display.mdAndUp
+                ? 'padding: 6rem 0 6rem 0'
+                : 'padding: 2rem 0 4rem 0'
+            "
           >
             <v-row no-gutters justify="center">
-              <v-col cols="8">
+              <v-col cols="12" md="8" class="px-6 px-md-0">
                 <h1 class="title-text-sub my-6">Our Branches</h1>
-                <p class="title-text-sub1">
+                <p
+                  :class="
+                    $vuetify.display.smAndDown ? 'text-small' : 'text-body-2'
+                  "
+                >
                   Founded by the Singapore-based company Seven365, expand our
                   commitment to innovation globally.
                 </p>
@@ -28,44 +39,75 @@
               </v-col>
             </v-row>
           </v-col>
-          <v-col cols="6" style="padding: 6rem 0 6rem 0">
-            <v-row no-gutters>
-              <v-col cols="5" style="border-right: 1px solid gray">
-                <v-row no-gutters>
-                  <v-col cols="12" class="mb-6">
-                    <v-img src="/images/singapore.png" cover width="70"></v-img>
-                  </v-col>
-                  <v-col cols="12" class="d-flex flex-column mb-6">
-                    <span class="text-h5">Seven365 Pte. Ltd</span>
-                    <span class="text-subtitle-1">Singapore Branch</span>
-                  </v-col>
+          <v-col
+            cols="12"
+            md="7"
+            lg="6"
+            :style="
+              $vuetify.display.mdAndUp
+                ? 'padding: 6rem 0 6rem 0'
+                : 'padding: 0rem 0 6rem 0'
+            "
+          >
+            <v-row no-gutters align="center" class="px-6 px-md-0">
+              <v-col
+                cols="12"
+                md="5"
+              >
+                <v-row no-gutters align="center">
+                  <div
+                    class="d-flex flex-row align-center justify-start ga-2 mb-4 mb-md-0"
+                  >
+                    <div class="mb-md-6">
+                      <v-img
+                        src="/images/singapore.png"
+                        cover
+                        :width="$vuetify.display.lgAndUp ? 70 : 60"
+                      ></v-img>
+                    </div>
+                    <div class="d-flex flex-column mb-md-6">
+                      <span class="text-h6">Seven365 Pte. Ltd</span>
+                      <span class="text-subtitle-1">Singapore Branch</span>
+                    </div>
+                  </div>
                   <v-col cols="12">
-                    <span class="text-subtitle-1">
-                      Blk 164 Bukit Merah Central <br />
-                      #03-3627 Singapore.<br />
+                    <span class="text-small">
+                      Blk 164 Bukit Merah Central
+                      <br v-if="$vuetify.display.mdAndUp" />
+                      #03-3627 Singapore.<br v-if="$vuetify.display.mdAndUp" />
                       150164
                     </span>
                   </v-col>
                 </v-row>
               </v-col>
+              <v-divider
+                v-if="$vuetify.display.smAndDown"
+                class="my-6"
+              ></v-divider>
               <v-spacer></v-spacer>
-              <v-col cols="5">
-                <v-row no-gutters>
-                  <v-col cols="12" class="mb-5" style="margin-top: -3px">
-                    <v-img
-                      src="/images/philippines.png"
-                      cover
-                      width="70"
-                    ></v-img>
-                  </v-col>
-                  <v-col cols="12" class="d-flex flex-column mb-6">
-                    <span class="text-h5">Softcode365 Inc.</span>
-                    <span class="text-subtitle-1">Philippines Branch</span>
-                  </v-col>
+              <v-col cols="12" md="6">
+                <v-row no-gutters align="center">
+                  <div
+                    class="d-flex flex-row align-center justify-start ga-2 mb-4 mb-md-0"
+                  >
+                    <div class="mb-md-6" style="margin-top: -3px">
+                      <v-img
+                        src="/images/philippines.png"
+                        cover
+                        :width="$vuetify.display.lgAndUp ? 70 : 60"
+                      ></v-img>
+                    </div>
+                    <div class="d-flex flex-column mb-md-6">
+                      <span class="text-h6">Softcode365 Inc.</span>
+                      <span class="text-subtitle-1">Philippines Branch</span>
+                    </div>
+                  </div>
                   <v-col cols="12">
-                    <span class="text-subtitle-1">
-                      Blk 30 Lot 10 Phase II, Pinagsama <br />
-                      Village, Taguig City, Metro Manila <br />
+                    <span class="text-small">
+                      Blk 30 Lot 10 Phase II, Pinagsama
+                      <br v-if="$vuetify.display.mdAndUp" />
+                      Village, Taguig City, Metro Manila
+                      <br v-if="$vuetify.display.mdAndUp" />
                       Philippines 1630​
                     </span>
                   </v-col>
@@ -80,7 +122,7 @@
         cover
         width="250"
         class="position-absolute mb-1 bottom-0"
-        style="right: -4rem"
+        :style="$vuetify.display.mdAndUp ? 'right: -4rem' : 'right: 0rem;'"
       ></v-img>
     </v-row>
   </div>
