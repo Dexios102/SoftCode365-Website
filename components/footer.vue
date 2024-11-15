7c96a73c5de4b9e8b569daa961e14f869dbe69e9
githubSeven365 <dexterdave.cajayon.1121@gmail.com>
2024-11-15 13:56:16 +0800

    services responsive

--- components/footer.vue
+++ components/footer.vue
@@ -1,22 +1,34 @@
 <template>
   <v-container fluid>
-    <v-row no-gutters class="padding-horizontal-default">
+    <v-row
+      no-gutters
+      class="padding-horizontal-default"
+      :class="
+        $vuetify.display.lgAndUp
+          ? 'padding-horizontal-default'
+          : $vuetify.display.md
+          ? 'px-6'
+          : 'px-8'
+      "
+    >
       <v-col cols="12" class="border-t-md py-12">
         <v-row no-gutters>
-          <v-col cols="4" class="d-flex flex-column ga-4">
+          <v-col cols="12" md="4" class="d-flex flex-column ga-4 mb-4 mb-md-0">
             <v-img
               src="/images/logo/softcode365logo.svg"
-              width="200"
-              cover
+              width="50%"
             ></v-img>
-            <p style="width: 80%">
+            <p
+              :style="$vuetify.display.mdAndUp ? 'width: 80%' : 'width: 100%'"
+              class="text-small"
+            >
               We are committed to delivering exceptional quality in all aspects
               of our services. We strive for excellence in every task we
               undertake, setting high standards for ourselves and consistently
               exceeding expectations.
             </p>
           </v-col>
-          <v-col cols="4">
+          <v-col cols="12" md="4" class="mb-4 mb-md-0">
             <span class="text-blue-theme font-weight-medium w-100"
               >Services</span
             >
@@ -35,19 +47,19 @@
               </v-col>
             </v-row>
           </v-col>
-          <v-col cols="4">
+          <v-col cols="12" md="4">
             <v-row no-gutters>
-              <v-col cols="8">
+              <v-col cols="12" sm="6" md="8" class="mb-4 mb-md-0">
                 <span class="text-blue-theme font-weight-medium"
                   >Our Branches</span
                 >
                 <v-row no-gutters class="mt-4">
                   <v-col cols="12" class="mb-4">
                     <v-row no-gutters>
-                      <v-col cols="3">
+                      <v-col cols="3" sm="2">
                         <v-img src="/images/singapore.png" width="50"></v-img>
                       </v-col>
-                      <v-col cols="9" class="d-flex flex-column">
+                      <v-col cols="9" sm="10" class="d-flex flex-column">
                         <span> Seven365 Pte. Ltd </span>
                         <span class="text-caption"> Singapore Branch </span>
                       </v-col>
@@ -55,10 +67,10 @@
                   </v-col>
                   <v-col cols="12">
                     <v-row no-gutters>
-                      <v-col cols="3">
+                      <v-col cols="3" sm="2">
                         <v-img src="/images/philippines.png" width="50"></v-img>
                       </v-col>
-                      <v-col cols="9" class="d-flex flex-column">
+                      <v-col cols="9" sm="10" class="d-flex flex-column">
                         <span> Softcode365 Inc. </span>
                         <span class="text-caption"> Philippines Branch </span>
                       </v-col>
@@ -66,8 +78,8 @@
                   </v-col>
                 </v-row></v-col
               >
-              <v-col cols="4">
-                <span class="text-blue-theme font-weight-medium pl-4"
+              <v-col cols="12" sm="6" md="4" class="mb-4 mb-md-0">
+                <span class="text-blue-theme font-weight-medium"
                   >Follow us on</span
                 >
                 <v-row no-gutters class="mt-4">
@@ -83,6 +95,9 @@
                         variant="text"
                         class="text-capitalize font-weight-medium"
                         color="black"
+                        style="
+                          padding-left: 0 !important
+                        "
                       >
                         <v-icon size="large" class="mr-3">mdi-facebook</v-icon>
                         Facebook
@@ -93,6 +108,9 @@
                         variant="text"
                         class="text-capitalize font-weight-medium"
                         color="black"
+                        style="
+                          padding-left: 0 !important
+                        "
                       >
                         <v-icon size="large" class="mr-3">mdi-twitter</v-icon>
                         Twitter
@@ -103,6 +121,9 @@
                         variant="text"
                         class="text-capitalize font-weight-medium"
                         color="black"
+                        style="
+                          padding-left: 0 !important
+                        "
                       >
                         <v-icon size="large" class="mr-3">mdi-instagram</v-icon>
                         Instagram
@@ -116,6 +137,9 @@
                         variant="text"
                         class="text-capitalize font-weight-medium"
                         color="black"
+                        style="
+                          padding-left: 0 !important
+                        "
                       >
                         <v-icon size="large" class="mr-3">mdi-linkedin</v-icon>
                         LinkedIn
@@ -128,22 +152,38 @@
           </v-col>
         </v-row>
       </v-col>
-      <v-col cols="12" class="border-t-md py-6">
+      <v-col cols="12" class="border-t-md py-6 mb-4 mb-md-0">
         <v-row no-gutters align="center">
-          <v-col cols="8">
+          <v-col cols="12" md="8">
             <span class="text-subtitle-2 font-weight-medium"
               >Copyright © 2024. Softcode365 Inc. All rights reserved.</span
             >
           </v-col>
-          <v-col cols="4" align="end">
+          <v-col
+            cols="12"
+            md="4"
+            :align="$vuetify.display.mdAndUp ? 'end' : 'start'"
+          >
             <v-row no-gutters>
-              <v-col cols="6">
-                <v-btn variant="text" class="text-capitalize" color="#a6282d"
+              <v-col cols="12" sm="6">
+                <v-btn
+                  variant="text"
+                  class="text-capitalize"
+                  color="#a6282d"
+                  :style="
+                    $vuetify.display.smAndDown && 'padding-left: 0 !important'
+                  "
                   >Terms & Conditions</v-btn
                 >
               </v-col>
-              <v-col cols="6">
-                <v-btn variant="text" class="text-capitalize" color="#a6282d"
+              <v-col cols="12" sm="6">
+                <v-btn
+                  variant="text"
+                  class="text-capitalize"
+                  color="#a6282d"
+                  :style="
+                    $vuetify.display.smAndDown && 'padding-left: 0 !important'
+                  "
                   >Privacy Policy</v-btn
                 >
               </v-col>
--- pages/services/index.vue
+++ pages/services/index.vue
@@ -4,7 +4,14 @@
       <v-col cols="12" justify="center">
         <v-row
           no-gutters
-          class="padding-horizontal-default justify-center font-weight-medium"
+          class="justify-center font-weight-medium"
+          :class="
+            $vuetify.display.lgAndUp
+              ? 'padding-horizontal-default'
+              : $vuetify.display.md
+              ? 'px-6'
+              : 'px-8'
+          "
         >
           <v-col cols="12" class="mb-8">
             <v-row
@@ -14,19 +21,56 @@
               <v-col cols="12" justify="center">
                 <v-row no-gutters align="center">
                   <v-col
-                    cols="6"
+                    cols="12"
+                    md="6"
                     class="d-flex align-center"
-                    style="padding: 6rem 0"
+                    :order="$vuetify.display.mdAndUp ? '1' : '2'"
+                    :style="
+                      $vuetify.display.mdAndUp
+                        ? 'padding: 6rem 0'
+                        : 'padding: 4rem 1rem'
+                    "
                   >
                     <v-row no-gutters justify="center">
-                      <v-col cols="8">
+                      <v-col
+                        cols="12"
+                        md="8"
+                        :class="
+                          $vuetify.display.mdAndUp
+                            ? 'text-small'
+                            : 'text-center text-small'
+                        "
+                        class="position-relative"
+                      >
                         <h1 class="title-text-sub my-6">Customer Service</h1>
+                        <p class="text-small">
+                          At Softcode365 Inc., we deeply appreciate the profound
+                          impact that exceptional customer service has on
+                          fostering enduring relationships, nurturing loyalty,
+                          and propelling business growth.
+                        </p>
+                        <v-img
+                          src="/images/hero-images/circuit-frame-horizontal.svg"
+                          cover
+                          width="200"
+                          class="position-absolute"
+                          style="right: 0rem; bottom: -5rem"
+                          v-if="$vuetify.display.smAndDown"
+                        />
                       </v-col>
                     </v-row>
                   </v-col>
-                  <v-col cols="6">
-                    <v-row no-gutters>
-                      <v-col cols="12" class="mt-10 position-relative">
+                  <v-col
+                    cols="12"
+                    md="6"
+                    :order="$vuetify.display.mdAndUp ? '2' : '1'"
+                  >
+                    <v-row no-gutters justify="center">
+                      <v-col
+                        cols="12"
+                        class="mt-10 position-relative"
+                        :align="$vuetify.display.smAndDown && 'center'"
+                      >
                         <v-img
                           src="/icons/customer-service.png"
                           width="250"
@@ -38,6 +82,7 @@
                           width="300"
                           class="position-absolute"
                           style="right: -4rem; bottom: 4rem"
+                          v-if="$vuetify.display.mdAndUp"
                         />
                       </v-col>
                     </v-row>
@@ -48,7 +93,7 @@
           </v-col>
           <v-col cols="12">
             <v-row no-gutters>
-              <v-col cols="6" class="d-flex align-stretch">
+              <v-col cols="12" md="6" class="d-flex align-stretch">
                 <ServiceCard
                   title="Customer Services"
                   imgSrc="/icons/mobile1.png"
@@ -59,7 +104,7 @@
                   growth.
                 </ServiceCard>
               </v-col>
-              <v-col cols="6" class="d-flex align-stretch">
+              <v-col cols="12" md="6" class="d-flex align-stretch">
                 <ServiceCard
                   title="Software Management"
                   imgSrc="/icons/mobile1.png"
@@ -72,7 +117,7 @@
               </v-col>
             </v-row>
             <v-row no-gutters>
-              <v-col cols="4" class="d-flex align-stretch">
+              <v-col cols="12" md="4" class="d-flex align-stretch">
                 <ServiceCard
                   title="Cloud Hosting Services"
                   imgSrc="/icons/mobile1.png"
@@ -83,14 +128,14 @@
                   us for superior cloud hosting solutions.
                 </ServiceCard>
               </v-col>
-              <v-col cols="4" class="d-flex align-stretch">
+              <v-col cols="12" md="4" class="d-flex align-stretch">
                 <ServiceCard title="E-Commerce" imgSrc="/icons/mobile1.png">
                   We are aware of the distinctive challenges and opportunities
                   within the e-commerce realm, and we provide a diverse array of
                   services to assist your online business ventures.
                 </ServiceCard>
               </v-col>
-              <v-col cols="4" class="d-flex align-stretch">
+              <v-col cols="12" md="4" class="d-flex align-stretch">
                 <ServiceCard title="Telemarketing" imgSrc="/icons/mobile1.png">
                   With our expertise in telemarketing, we employ unique
                   strategies tailored to your business goals, ensuring optimal
