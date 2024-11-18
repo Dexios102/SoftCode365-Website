2d4dda57a29b737bd43e90e3ee02f5f36ba65ad1
githubSeven365 <dexterdave.cajayon.1121@gmail.com>
2024-11-18 13:39:37 +0800

    contact page responsive

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -3,7 +3,14 @@
     <v-col cols="12" justify="center">
       <v-row
         no-gutters
-        class="padding-horizontal-default justify-center font-weight-medium"
+        class="justify-center font-weight-medium"
+        :class="
+            $vuetify.display.lgAndUp
+              ? 'padding-horizontal-default'
+              : $vuetify.display.md
+              ? 'px-6'
+              : 'px-8'
+          "
       >
         <v-col cols="12">
           <v-row no-gutters justify="center">
@@ -19,22 +26,24 @@
         </v-col>
         <v-row
           no-gutters
-          class="border my-10 rounded-lg pl-2 py-2"
+          class="border my-10 pl-2 rounded-lg py-2 pr-2 pr-md-0"
           style="box-shadow: 0px 8.57px 24px 0px #0000000d"
           align="center"
         >
           <v-col
-            cols="4"
-            class="border bg-dark-theme rounded-lg pa-6 text-white"
+            cols="12"
+            md="4"
+            class="border bg-dark-theme pa-6 text-white ma-2 ma-md-0"
+            :class="$vuetify.display.smAndDown ? 'rounded-xl' : 'rounded-lg'"
           >
             <v-row no-gutters class="d-flex flex-column ga-10">
               <v-col cols="12" class="d-flex flex-column ga-4 mt-8">
                 <span class="text-l">Reach us for inquiries</span>
                 <p class="text-small font-weight-regular">
-                  Contact us today to schedule a consultation <br />
-                  and learn more about our comprehensive <br />
-                  range of services. Let us be your trusted <br />
-                  partner in success.
+                  Contact us today to schedule a consultation <br v-if="$vuetify.display.mdAndUp" />
+                  and learn more about our comprehensive <br v-if="$vuetify.display.mdAndUp" />
+                  range of services. Let us be your trusted <br v-if="$vuetify.display.mdAndUp" />
+                  partner in success. 
                 </p>
               </v-col>
               <v-col cols="12">
@@ -110,18 +119,18 @@
               </v-col>
             </v-row>
           </v-col>
-          <v-col cols="8" class="pa-14 position-relative">
-            <v-row no-gutters class="py-16">
+          <v-col cols="12" md="8" class="pa-4 pa-md-14 position-relative">
+            <v-row no-gutters class="py-md-16">
               <v-col cols="12">
                 <v-row no-gutters>
-                  <v-col cols="6" class="pr-6">
+                  <v-col cols="12" md="6" class="pt-2 pt-md-0 pr-md-6">
                     <v-text-field
                       clearable
                       label="Subject"
                       variant="underlined"
                     ></v-text-field>
                   </v-col>
-                  <v-col cols="6" class="pl-6">
+                  <v-col cols="12" md="6" class="pt-2 pt-md-0 pl-md-6">
                     <v-text-field
                       clearable
                       label="Full name"
@@ -133,7 +142,7 @@
               </v-col>
               <v-col cols="12">
                 <v-row no-gutters>
-                  <v-col cols="6" class="pr-6">
+                  <v-col cols="12" md="6" class="pt-2 pt-md-0 pr-md-6">
                     <v-text-field
                       clearable
                       label="Email"
@@ -141,7 +150,7 @@
                       placeholder="johndoe@gmail.com"
                     ></v-text-field>
                   </v-col>
-                  <v-col cols="6" class="pl-6">
+                  <v-col cols="12" md="6" class="pt-2 pt-md-0 pl-md-6">
                     <v-text-field
                       clearable
                       label="Phone Number"
@@ -150,7 +159,7 @@
                   </v-col>
                 </v-row>
               </v-col>
-              <v-col cols="12">
+              <v-col cols="12" class="pt-2 pt-md-0">
                 <v-text-field
                   clearable
                   label="Message"
@@ -158,11 +167,12 @@
                   placeholder="Write your message..."
                 ></v-text-field
               ></v-col>
-              <v-col cols="12" align="end">
+              <v-col cols="12" align="end" class="pt-4 pt-md-0">
                 <v-btn
                   size="x-large"
                   class="rounded-xl text-capitalize"
                   color="#a6282d"
+                  :class="$vuetify.display.mdAndUp ? 'w-auto' : 'w-100 text-caption'"
                 >
                   Send Message
                 </v-btn>
@@ -174,6 +184,7 @@
               width="300"
               class="position-absolute bottom-0 mb-1"
               style="right: 0rem"
+              v-if="$vuetify.display.mdAndUp"
             ></v-img>
             <v-img
               src="/images/circuit-black-up.svg"
@@ -181,6 +192,7 @@
               width="300"
               class="position-absolute top-0 mb-1"
               style="right: 0rem"
+              v-if="$vuetify.display.mdAndUp"
             ></v-img>
           </v-col>
         </v-row>
